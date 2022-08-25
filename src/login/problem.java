6baf50e2c9f6d04d2f16ae7244c6a35de38b4bbb
enas <enaselsaffar81@gmail.com>
2022-08-25 12:40:21 -0500

    problem class create

---
+++ src/login/problem.java
@@ -0,0 +1,17 @@
+package login;
+
+public class problem {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println(i);
+        }
+
+        //some code here
+        //another code here
+
+
+
+
+
+    }
+}
