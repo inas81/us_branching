8990377257bc391bd866509b20b98858b9d49f52
enas <enaselsaffar81@gmail.com>
2022-08-25 20:54:48 -0500

    creat case1 f10

---
+++ src/login/Case.java
@@ -0,0 +1,14 @@
+package login;
+
+public class Case {
+    public static void main(String[] args) {
+        for (int i = 0; i <8 ; i++) {
+
+            System.out.println(i);
+        }
+
+
+
+        //import code
+    }
+}
